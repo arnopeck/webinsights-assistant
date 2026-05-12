0916f028c3f5f1fbfca96718cfa709bcc9f91b54
arnopeck <arnopeck@gmail.com>
2026-05-12 03:03:34 +0200

    Remove obsolete Python package marker

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-WebInsights Assistant - Test Initialization
-
-Questo script crea un file __init__.py vuoto per consentire l'importazione
-dei moduli di test come pacchetto.
-"""
-
-# File vuoto per consentire l'importazione dei moduli di test
